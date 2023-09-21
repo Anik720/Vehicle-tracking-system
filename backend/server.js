81a3f48824645882bdebc95ad0e1c96e980924e5
Anik720 <heanik39@gmail.com>
2023-09-21 21:53:42 +0600

    completed the project

---
+++ backend/server.js
@@ -0,0 +1,128 @@
+const express = require("express");
+const cors = require("cors");
+const http = require("http");
+const path = require("path");
+const { Server } = require("socket.io");
+
+const app = express();
+app.use(cors());
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*", // adjust this according to your needs
+    methods: ["GET", "POST"],
+  },
+});
+
+app.use(express.json());
+
+let vehicleData = [
+  { id: 1, status: "moving", coordinates: [51.505, -0.09], distance: 0 },
+  { id: 2, status: "idle", coordinates: [51.51, -0.1], distance: 0 },
+  { id: 3, status: "idle", coordinates: [51.51, -0.1], distance: 0 },
+  { id: 4, status: "idle", coordinates: [51.51, -0.1], distance: 0 },
+  { id: 5, status: "idle", coordinates: [51.51, -0.1], distance: 0 },
+];
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("updateVehicle", (data) => {
+    let vehicle = vehicleData.find((v) => v.id === data.id);
+    if (vehicle) {
+      vehicle.status = data.status;
+      vehicle.coordinates = data.coordinates;
+      vehicle.distance = data.distance;
+
+      // Broadcast the update to all clients
+      io.emit("vehicleUpdated", vehicle);
+    }
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+  });
+});
+
+app.get("/vehicles", (req, res) => {
+  res.json(vehicleData);
+});
+
+app.get("/socket.io/socket.io.js", (req, res) => {
+  res.sendFile(
+    path.resolve(
+      __dirname,
+      "node_modules/socket.io/client-dist/socket.io.min.js"
+    )
+  );
+});
+
+app.post("/update-vehicle", (req, res) => {
+  let { id, status, coordinates, distance } = req.body;
+
+  let vehicle = vehicleData.find((v) => v.id === id);
+  if (vehicle) {
+    vehicle.status = status;
+    vehicle.coordinates = coordinates;
+    vehicle.distance = distance;
+
+    res.json({ message: "Vehicle data updated successfully", vehicle });
+  } else {
+    res.status(404).json({ message: "Vehicle not found" });
+  }
+});
+
+const PORT = 3000;
+
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+function calculateDistance(coord1, coord2) {
+  function toRad(value) {
+    return (value * Math.PI) / 180;
+  }
+
+  var lat1 = coord1[0];
+  var lon1 = coord1[1];
+  var lat2 = coord2[0];
+  var lon2 = coord2[1];
+
+  var R = 6371; // Radius of Earth in kilometers
+  var dLat = toRad(lat2 - lat1);
+  var dLon = toRad(lon2 - lon1);
+  var a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(toRad(lat1)) *
+      Math.cos(toRad(lat2)) *
+      Math.sin(dLon / 2) *
+      Math.sin(dLon / 2);
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  var d = R * c;
+  
+  return d;
+}
+
+function simulateUpdates() {
+  setInterval(() => {
+    vehicleData.forEach((vehicle) => {
+      // Save previous coordinates
+      let prevCoordinates = [...vehicle.coordinates];
+
+      // Update the vehicle data (here it's just simulating a small change in coordinates)
+      vehicle.coordinates[0] += Math.random() * 0.01 - 0.005;
+      vehicle.coordinates[1] += Math.random() * 0.01 - 0.005;
+
+      // Randomly update the status
+      vehicle.status = Math.random() > 0.5 ? "moving" : "idle";
+
+      // Calculate the distance using the Haversine formula and add to the total distance
+      let distance = calculateDistance(prevCoordinates, vehicle.coordinates);
+      vehicle.distance += distance;
+
+      // Emit the updated vehicle data
+      io.emit("vehicleUpdated", vehicle);
+    });
+  }, 3000); // Change interval as needed
+}
+simulateUpdates()
---
+++ frontend/script.js
@@ -0,0 +1,301 @@
+let map = L.map("map").setView([51.505, -0.09], 13);
+
+// L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+//   attribution:
+//     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+// }).addTo(map);
+L.tileLayer(
+  "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
+  {
+    attribution: "Tiles &copy; Esri",
+  }
+).addTo(map);
+let vehicleDistanceChartData = {};
+let vehicleDistanceChart;
+
+google.charts.load("current", { packages: ["corechart"] });
+google.charts.setOnLoadCallback(initializeChart);
+
+function initializeChart() {
+  vehicleDistanceChart = new google.visualization.LineChart(
+    document.getElementById("vehicleDistanceChart")
+  );
+  drawChart([]);
+}
+
+// function drawChart(data) {
+//   let dataTable = new google.visualization.DataTable();
+//   dataTable.addColumn("datetime", "Time");
+//   dataTable.addColumn("number", "Distance");
+//   dataTable.addRows(data);
+
+//   let options = {
+//     title: "Vehicle Distance Over Time",
+//     curveType: "function",
+//     legend: { position: "bottom" },
+//     hAxis: { title: "Time" },
+//     vAxis: { title: "Distance" },
+//   };
+
+//   vehicleDistanceChart.draw(dataTable, options);
+// }
+
+// function updateVehicleDistanceChart(vehicle) {
+//   let currentTime = new Date();
+
+//   if (!vehicleDistanceChartData[vehicle.id]) {
+//     vehicleDistanceChartData[vehicle.id] = [];
+//   }
+
+//   vehicleDistanceChartData[vehicle.id].push([currentTime, vehicle.distance]);
+
+//   drawChart(vehicleDistanceChartData[vehicle.id]);
+// }
+
+function drawChart(data) {
+  if (data.length === 0) {
+    return; // Return early if data is empty
+  }
+  let dataTable = new google.visualization.DataTable();
+
+  dataTable.addColumn("datetime", "Time");
+
+  // Find unique vehicle IDs
+  let vehicleIds = [...new Set(data.map((item) => item[2]))]; // Changed item[0] to item[2] to get vehicleId
+
+  // Add a data column for each vehicle
+  vehicleIds.forEach((vehicleId) => {
+    dataTable.addColumn("number", `Vehicle ${vehicleId}`);
+  });
+
+  // Group data by time
+  let groupedData = data.reduce((acc, [time, distance, vehicleId]) => {
+    if (!acc[time]) {
+      acc[time] = { time, distances: {} };
+    }
+    acc[time].distances[vehicleId] = distance;
+    return acc;
+  }, {});
+
+  // Convert grouped data to rows
+  let rows = Object.values(groupedData).map(({ time, distances }) => {
+    let row = [new Date(time)]; // Convert string time to Date object
+    vehicleIds.forEach((vehicleId) => {
+      row.push(distances[vehicleId] || null); // Ensure a value (even if null) is pushed for every vehicle
+    });
+    return row;
+  });
+
+  dataTable.addRows(rows);
+
+  let options = {
+    title: "Vehicle Distance Over Time",
+    curveType: "function",
+    legend: { position: "bottom" },
+    hAxis: { title: "Time" },
+    vAxis: { title: "Distance" },
+  };
+
+  vehicleDistanceChart.draw(dataTable, options);
+}
+
+function updateVehicleDistanceChart(vehicle) {
+  let currentTime = new Date();
+
+  if (!vehicleDistanceChartData[vehicle.id]) {
+    vehicleDistanceChartData[vehicle.id] = [];
+  }
+
+  vehicleDistanceChartData[vehicle.id].push([
+    currentTime.toISOString(),
+    vehicle.distance,
+    vehicle.id,
+  ]); // Added vehicle.id to the data
+
+  // Collect data from all vehicles
+  let allVehicleData = [];
+  for (let vehicleId in vehicleDistanceChartData) {
+    allVehicleData.push(...vehicleDistanceChartData[vehicleId]);
+  }
+
+  drawChart(allVehicleData);
+}
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  let socket = io.connect("http://localhost:3000");
+
+  socket.on("vehicleUpdated", (data) => {
+    updateVehicleData(data);
+  });
+
+  fetch("http://localhost:3000/vehicles")
+    .then((response) => response.json())
+    .then((data) => {
+      data.forEach((vehicle) => {
+        addVehicleToMap(vehicle);
+        addVehicleToList(vehicle);
+
+        // updateTable(data);
+      });
+    })
+    .catch((error) => {
+      console.error("Error fetching vehicles:", error);
+    });
+
+  document
+    .getElementById("filter-moving")
+    .addEventListener("change", updateFilters);
+  document
+    .getElementById("filter-idle")
+    .addEventListener("change", updateFilters);
+});
+
+let vehicleMarkers = {};
+let vehicleListItems = {};
+
+function updateVehicleData(vehicle) {
+  // Find the vehicle marker and list item and update them with the new data
+  let marker = vehicleMarkers[vehicle.id];
+  let listItem = vehicleListItems[vehicle.id];
+
+  if (marker) {
+    marker.setLatLng(vehicle.coordinates);
+    marker.bindPopup(
+      `Vehicle ID: ${vehicle.id}<br>Status: ${vehicle.status}<br>Distance: ${vehicle.distance}`
+    );
+    marker.vehicleData = vehicle;
+  }
+
+  if (listItem) {
+    listItem.innerHTML = `Vehicle ID: ${vehicle.id} <span>Status: ${vehicle.status}</span> <span>Distance: ${vehicle.distance} km</span>`;
+    listItem.vehicleData = vehicle;
+  }
+  updateVehicleDistanceChart(vehicle);
+
+  // Also, update the table here with the latest data
+  fetch("http://localhost:3000/vehicles")
+    .then((response) => response.json())
+    .then((data) => {
+      const filterMovingCheckbox = document.getElementById("filter-moving");
+      const filterIdleCheckbox = document.getElementById("filter-idle");
+
+      // Check the checkboxes based on some condition (you can customize this)
+      if (
+        filterMovingCheckbox.checked === true ||
+        filterIdleCheckbox.checked === true
+      ) {
+        applyFilters();
+      } else {
+        updateTable(data);
+      }
+    })
+    .catch((error) => {
+      console.error("Error fetching vehicles:", error);
+    });
+}
+
+function addVehicleToMap(vehicle) {
+  let marker = L.marker(vehicle.coordinates).addTo(map);
+  marker.vehicleData = vehicle;
+  
+  marker.bindTooltip(`Vehicle ID: ${vehicle.id}`, { permanent: true, direction: 'top' }).openTooltip();
+  
+  marker.bindPopup(
+    `Vehicle ID: ${vehicle.id}<br>Status: ${vehicle.status}<br>Distance: ${vehicle.distance}`
+  );
+  vehicleMarkers[vehicle.id] = marker;
+}
+// Assuming data is an array of objects with properties: vehicleID, status, and distance
+function updateTable(data) {
+  const tbody = document.querySelector("#vehicle-list tbody");
+  tbody.innerHTML = ""; // Clear the existing rows
+
+  data.forEach((item) => {
+    const row = document.createElement("tr");
+
+    const cell1 = document.createElement("td");
+    cell1.style.width = "30%"; // Apply styles to the new td elements
+    cell1.textContent = item.id; // Changed vehicleID to id
+
+    const cell2 = document.createElement("td");
+    cell2.style.width = "40%"; // Apply styles to the new td elements
+    cell2.textContent = item.status;
+
+    const cell3 = document.createElement("td");
+    cell3.style.width = "30%"; // Apply styles to the new td elements
+    cell3.textContent = item.distance; // Removed km as it is now appended in list item
+
+    row.appendChild(cell1);
+    row.appendChild(cell2);
+    row.appendChild(cell3);
+
+    tbody.appendChild(row);
+  });
+}
+// function addVehicleToList(vehicle) {
+//   let vehicleList = document.getElementById("vehicle-list");
+//   let vehicleItem = document.createElement("div");
+//   vehicleItem.className = "vehicle-item";
+//   vehicleItem.vehicleData = vehicle;
+//   vehicleItem.innerHTML = `Vehicle ID: ${vehicle.id} <span>Status: ${vehicle.status}</span> <span>Distance: ${vehicle.distance}</span>`;
+//   vehicleList.appendChild(vehicleItem);
+//   vehicleListItems[vehicle.id] = vehicleItem;
+// }
+function addVehicleToList(vehicle) {
+  let vehicleList = document.querySelector("#vehicle-list tbody");
+  let vehicleItem = document.createElement("tr");
+  vehicleItem.vehicleData = vehicle;
+
+  let vehicleIdCell = document.createElement("td");
+  vehicleIdCell.textContent = vehicle.id;
+
+  let vehicleStatusCell = document.createElement("td");
+  vehicleStatusCell.textContent = vehicle.status;
+
+  let vehicleDistanceCell = document.createElement("td");
+  vehicleDistanceCell.textContent = vehicle.distance + " km";
+
+  vehicleItem.appendChild(vehicleIdCell);
+  vehicleItem.appendChild(vehicleStatusCell);
+  vehicleItem.appendChild(vehicleDistanceCell);
+
+  vehicleList.appendChild(vehicleItem);
+  vehicleListItems[vehicle.id] = vehicleItem;
+}
+
+function applyFilters() {
+  let showMoving = document.getElementById("filter-moving").checked;
+  let showIdle = document.getElementById("filter-idle").checked;
+
+  // for (let id in vehicleMarkers) {
+  //   let vehicle = vehicleMarkers[id].vehicleData;
+  //   let shouldShow =
+  //     (showMoving && vehicle.status === "moving") ||
+  //     (showIdle && vehicle.status === "idle");
+
+  //   if (shouldShow) {
+  //     vehicleMarkers[id].addTo(map);
+  //     vehicleListItems[id].style.display = "";
+  //   } else {
+  //     vehicleMarkers[id].remove();
+  //     vehicleListItems[id].style.display = "none";
+  //   }
+  // }
+
+  let tableRows = document.querySelectorAll("#vehicle-list tbody tr");
+  tableRows.forEach((row) => {
+    let status = row.children[1].textContent.toLowerCase();
+    let shouldShow =
+      (showMoving && status === "moving") || (showIdle && status === "idle");
+
+    if (shouldShow) {
+      row.style.display = "";
+    } else {
+      row.style.display = "none";
+    }
+  });
+}
+
+function updateFilters() {
+  applyFilters();
+}
